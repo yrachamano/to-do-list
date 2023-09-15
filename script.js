cd037ba2a5691e0a1f3e4deabf84ef35523cff50
daniel alves <daniel.andriola@gmail.com>
2023-09-15 11:56:02 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,55 +1,50 @@
 const btnAdicionar = document.getElementById('adicionar')
-const valorTarefa = document.getElementById('texto-input')
+const valorTarefa = document.getElementById('texto-input');
 
+function criarTarefa(valorTarefa) {
+    const li = document.createElement('li')
 
-btnAdicionar.addEventListener('click', (evento)=>{
-    evento.preventDefault();
-
- 
-
-if (valorTarefa.value == ''){
-    alert('Digite uma tarefa valida')
- } else{
-    const tarefa =  criarTarefa(valorTarefa.value)
- renderizarTarefa(tarefa)
+    li.innerHTML = `
+    <p>${valorTarefa}</p>
+    <div> 
+        <button class="excluir"></button>
+        <button class="concluir"></button>
+    </div>`
+    return li
 }
 
-valorTarefa.value = ''
+function renderizarTarefa(tarefa) {
+     const listaTarefas = document.querySelector('ul')
 
+     listaTarefas.appendChild(tarefa)
+}
+
+btnAdicionar.addEventListener('click', (evento) => {
+    evento.preventDefault();
+
+    if (valorTarefa.value == '') {
+        alert('Digite uma tarefa válida.')
+    }
+    else {
+        const tarefa = criarTarefa(valorTarefa.value)
+        renderizarTarefa(tarefa)
+    }       
+
+    valorTarefa.value = ''
 })
+
 
 const listaTarefas = document.querySelector('ul')
 
-listaTarefas.addEventListener('click',(elemento)=>{
+listaTarefas.addEventListener('click', (elemento) => {
+    const itemClicado = elemento.target
 
-const itemClicado = elemento.target
 
-if (itemClicado.classList.contains('excluir')){
-itemClicado.parentElement.parentElement.remove()
-}
+    if (itemClicado.classList.contains('excluir')){
+        itemClicado.parentElement.parentElement.remove()
+    }
+    if (itemClicado.classList.contains('concluir')){
+        itemClicado.parentElement.parentElement.classList.toggle('concluido')
+    }
 
-if (itemClicado.classList.contains('concluir')){
-    itemClicado.parentElement.parentElement.classList.toggle('concluido')
-}
-
-})
-
-function criarTarefa(valorTarefa){
-    
-    
-    const li = document.createElement('li')
-    li.innerHTML = `
-    ${valorTarefa}
-    <div>
-      <button class="excluir"></button>
-     <button class="concluir"></button>
- </div>`
- 
- return li
-}
-
-function renderizarTarefa(tarefa){
-    const listaTarefas = document.querySelector('ul')
-    
-    listaTarefas.appendChild(tarefa)
-}
+}) 
