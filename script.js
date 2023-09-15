02a7d657d6e9fccb4b07659956bc9a7e8fd83ec6
daniel alves <daniel.andriola@gmail.com>
2023-09-15 11:55:06 -0300

    atualizando

---
+++ script.js
@@ -0,0 +1,55 @@
+const btnAdicionar = document.getElementById('adicionar')
+const valorTarefa = document.getElementById('texto-input')
+
+
+btnAdicionar.addEventListener('click', (evento)=>{
+    evento.preventDefault();
+
+ 
+
+if (valorTarefa.value == ''){
+    alert('Digite uma tarefa valida')
+ } else{
+    const tarefa =  criarTarefa(valorTarefa.value)
+ renderizarTarefa(tarefa)
+}
+
+valorTarefa.value = ''
+
+})
+
+const listaTarefas = document.querySelector('ul')
+
+listaTarefas.addEventListener('click',(elemento)=>{
+
+const itemClicado = elemento.target
+
+if (itemClicado.classList.contains('excluir')){
+itemClicado.parentElement.parentElement.remove()
+}
+
+if (itemClicado.classList.contains('concluir')){
+    itemClicado.parentElement.parentElement.classList.toggle('concluido')
+}
+
+})
+
+function criarTarefa(valorTarefa){
+    
+    
+    const li = document.createElement('li')
+    li.innerHTML = `
+    ${valorTarefa}
+    <div>
+      <button class="excluir"></button>
+     <button class="concluir"></button>
+ </div>`
+ 
+ return li
+}
+
+function renderizarTarefa(tarefa){
+    const listaTarefas = document.querySelector('ul')
+    
+    listaTarefas.appendChild(tarefa)
+}
