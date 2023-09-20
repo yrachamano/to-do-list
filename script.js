795f87a39cee75101fdd3d25882a3475c1414163
daniel alves <daniel.andriola@gmail.com>
2023-09-20 10:58:25 -0300

    ta salvando em local

--- script.js
+++ script.js
@@ -1,57 +1,73 @@
 const btnAdicionar = document.getElementById('adicionar')
-const valorTarefa = document.getElementById('texto-input');
-var itensLista = []
-
-
-
-
-function criarTarefa(valorTarefa) {
-    const li = document.createElement('li')
-
-
-    li.innerHTML = `
-    <p>${valorTarefa}</p>
-    <div> 
-    <button class="excluir"></button>
-    <button class="concluir"></button>
-    </div>`
-    return li
-}
-
-function renderizarTarefa(tarefa) {
-     const listaTarefas = document.querySelector('ul')
-
-     listaTarefas.appendChild(tarefa)
-}
+let listaTarefas = document.querySelector('ul')
+const valorTarefa = document.getElementById('texto-input')
 
 btnAdicionar.addEventListener('click', (evento) => {
-    evento.preventDefault();
-
-    if (valorTarefa.value == '') {
-        alert('Digite uma tarefa válida.')
-    }
-    else {
+    evento.preventDefault()
+    
+    if (valorTarefa.value == '' ) {
+        alert('Digite uma tarefa válida')
+    } else {
+        escutador()
         const tarefa = criarTarefa(valorTarefa.value)
-        renderizarTarefa(tarefa)
-    }       
+        redereizarTarefa(tarefa)
+        salvadorMemoria()
+    }
 
     valorTarefa.value = ''
 })
 
+function criarTarefa (valorTarefa) {
+    const li = document.createElement('li')
+    li.innerHTML = `<span>
+    ${valorTarefa}</span>
+    <div>
+        <button class="excluir"></button>
+        <button class="concluir"></button>
+    </div>
+    `
+    return li
+}
 
-const listaTarefas = document.querySelector('ul')
+function redereizarTarefa (tarefa) {
+    const listaTarefas = document.querySelector('ul')
+    listaTarefas.appendChild(tarefa)
+}
 
-listaTarefas.addEventListener('click', (elemento) => {
-    const itemClicado = elemento.target
+function salvadorMemoria () {
+    let itensLista = listaTarefas.innerHTML
+    localStorage.setItem('itens', JSON.stringify(itensLista))
+}
 
+function escutador () {
+    listaTarefas.addEventListener('click', (elemento) => {
+        elemento.stopImmediatePropagation()
 
-    if (itemClicado.classList.contains('excluir')){
-        itemClicado.parentElement.parentElement.remove()
-    }
-    if (itemClicado.classList.contains('concluir')){
-        itemClicado.parentElement.parentElement.classList.toggle('concluido')
-    }
+        const itemclickado = elemento.target;
 
-}) 
+        if (itemclickado.parentElement.classList.contains('excluir')) {
+            itemclickado.parentElement.parentElement.parentElement.remove()
+            salvadorMemoria()
+        } else if (itemclickado.classList.contains('excluir')) {
+            itemclickado.parentElement.parentElement.remove()
+        } else if (itemclickado.parentElement.classList.contains('concluir')) {
+            if (itemclickado.parentElement.parentElement.parentElement.firstChild.classList.contains('concluido')) {
+                itemclickado.parentElement.parentElement.parentElement.firstChild.classList.remove('concluido')
+            } else {
+                itemclickado.parentElement.parentElement.parentElement.firstChild.classList.add('concluido')
+            }
+            
+            salvadorMemoria()
+        } else if (itemclickado.classList.contains('concluir')) {
+            itemclickado.parentElement.parentElement.firstChild.classList.toggle('concluido')
+            salvadorMemoria()
+        }
+    })
+}
 
-    
+if (localStorage.getItem('itens')) {
+    let codeInicio = JSON.parse(localStorage.getItem('itens'))
+    listaTarefas.innerHTML = codeInicio
+    listaTarefas = document.querySelector('ul')
+    escutador()
+}
